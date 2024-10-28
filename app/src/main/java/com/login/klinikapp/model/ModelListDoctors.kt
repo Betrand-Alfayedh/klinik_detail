9e0906ae04c156fb318ff52887a54b60973098fd
Betrand-Alfayedh <btrnd.alfydh@gmail.com>
2024-10-29 02:44:36 +0700

    first commit

---
+++ app/src/main/java/com/login/klinikapp/model/ModelListDoctors.kt
@@ -0,0 +1,9 @@
+package com.login.klinikapp.model
+
+data class ModelListDoctors(
+    var namaDr :String,
+    var imageDr :Int,
+    var bidangDr : String,
+    var totalReview : String,
+    var angkaReview : String
+)
